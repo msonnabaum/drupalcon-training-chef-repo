1ecbeeb619348dad10c5fe2a86aaed23c81a1a68
Mark Sonnabaum <mark@sonnabaum.com>
2012-03-11 14:54:22 -0500

    Bug fixes in the varnish cookbook, added some attributes as well.

--- cookbooks/varnish/attributes/default.rb
+++ cookbooks/varnish/attributes/default.rb
@@ -3,3 +3,6 @@ when "debian","ubuntu"
   set[:varnish][:dir]     = "/etc/varnish"
   set[:varnish][:default] = "/etc/default/varnish"
 end
+
+default[:varnish][:listen_port] = 6081
+default[:varnish][:admin_port] = 6082
--- cookbooks/varnish/recipes/default.rb
+++ cookbooks/varnish/recipes/default.rb
@@ -19,20 +19,6 @@
 
 package "varnish"
 
-template "#{node[:varnish][:dir]}default.vcl" do
-  source "default.vcl.erb"
-  owner "root"
-  group "root"
-  mode 0644
-end
-
-template "#{node[:varnish][:default]}" do
-  source "ubuntu-default.erb"
-  owner "root"
-  group "root"
-  mode 0644
-end
-
 service "varnish" do
   supports :restart => true, :reload => true
   action [ :enable, :start ]
@@ -41,4 +27,20 @@ end
 service "varnishlog" do
   supports :restart => true, :reload => true
   action [ :enable, :start ]
+end
+
+template "#{node[:varnish][:dir]}/default.vcl" do
+  source "default.vcl.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, resources(:service => "varnish")
+end
+
+template "#{node[:varnish][:default]}" do
+  source "ubuntu-default.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, resources(:service => "varnish")
 end
