b915f237aeaae91685b7a59fa1d3744496f2680c
Steven Merrill <steven.merrill@gmail.com>
2012-03-19 01:24:54 -0600

    Add a script to bootstrap a new server.

---
+++ bootstrap-server.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Installing Ruby and Ruby Gems through apt-get."
+
+# Install ruby and the ruby gems system through Ubuntu's package manager
+# (apt-get.)
+apt-get -y install rubygems
+# Ensure that the installed versions of all gems are up to date.
+gem update --system
+# Install the Chef gem.
+gem install chef
+
+echo "Installing Ruby and Ruby Gems through apt-get."
+
+# Install the Git version control system using Ubuntu's package manager.
+apt-get -y install git-core
+
+echo "You're ready to rock and roll."
+
